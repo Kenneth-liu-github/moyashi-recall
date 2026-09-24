278aad62accec3af9320489cc0b2af6dcec38d35
Kenneth-liu-github <84263465+Kenneth-liu-github@users.noreply.github.com>
2026-09-24 21:06:18 +0800

    feat: add Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MoyashiRecall",
+    platforms: [.iOS(.v17)],
+    products: [.library(name: "MoyashiRecall", targets: ["MoyashiRecall"])],
+    targets: [
+        .target(name: "MoyashiRecall"),
+        .testTarget(name: "MoyashiRecallTests", dependencies: ["MoyashiRecall"])
+    ]
+)
