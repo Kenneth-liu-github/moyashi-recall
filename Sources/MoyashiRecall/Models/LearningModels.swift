edc2e6f864a8ca19c7fb0105b2caae681e9f9522
Kenneth-liu-github <84263465+Kenneth-liu-github@users.noreply.github.com>
2026-09-24 22:20:41 +0800

    refactor: remove obsolete mock card model and counts

--- Sources/MoyashiRecall/Models/LearningModels.swift
+++ Sources/MoyashiRecall/Models/LearningModels.swift
@@ -5,44 +5,16 @@ public struct StudySource: Identifiable, Hashable {
     public let key: String
     public let title: String
     public let detail: String
-    public let itemCount: Int
 
     public init(
         id: UUID = UUID(),
         key: String,
         title: String,
-        detail: String,
-        itemCount: Int
+        detail: String
     ) {
         self.id = id
         self.key = key
         self.title = title
         self.detail = detail
-        self.itemCount = itemCount
-    }
-}
-
-public struct ReviewCard: Identifiable, Hashable {
-    public let id: UUID
-    public let prompt: String
-    public let answer: String
-    public let meaning: String
-    public let naturalEnglish: String
-    public let source: String
-
-    public init(
-        id: UUID = UUID(),
-        prompt: String,
-        answer: String,
-        meaning: String,
-        naturalEnglish: String,
-        source: String
-    ) {
-        self.id = id
-        self.prompt = prompt
-        self.answer = answer
-        self.meaning = meaning
-        self.naturalEnglish = naturalEnglish
-        self.source = source
     }
 }
