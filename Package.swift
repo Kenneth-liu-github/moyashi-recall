97147f8a01a655700ebcefc7cdc3f543ef632e56
Kenneth-liu-github <84263465+Kenneth-liu-github@users.noreply.github.com>
2026-09-24 22:41:03 +0800

    ci: add Linux core-validation package slice

--- Package.swift
+++ Package.swift
@@ -1,6 +1,35 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
+#if os(Linux)
+let package = Package(
+    name: "MoyashiRecall",
+    products: [
+        .library(name: "MoyashiRecall", targets: ["MoyashiRecall"])
+    ],
+    targets: [
+        .target(
+            name: "MoyashiRecall",
+            path: "Sources/MoyashiRecall",
+            sources: [
+                "Scheduling/ReviewLearningState.swift",
+                "Scheduling/FSRSScheduler.swift",
+                "Sources/LearningContentSource.swift",
+                "Sources/Notion/NotionAPIClient.swift"
+            ]
+        ),
+        .testTarget(
+            name: "MoyashiRecallTests",
+            dependencies: ["MoyashiRecall"],
+            path: "Tests/MoyashiRecallTests",
+            sources: [
+                "FSRSSchedulerTests.swift",
+                "NotionAPIClientTests.swift"
+            ]
+        )
+    ]
+)
+#else
 let package = Package(
     name: "MoyashiRecall",
     platforms: [
@@ -12,6 +41,10 @@ let package = Package(
     ],
     targets: [
         .target(name: "MoyashiRecall"),
-        .testTarget(name: "MoyashiRecallTests", dependencies: ["MoyashiRecall"])
+        .testTarget(
+            name: "MoyashiRecallTests",
+            dependencies: ["MoyashiRecall"]
+        )
     ]
 )
+#endif
