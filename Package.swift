f73cbb040c7fbd3b6c6538936bc4f9304c32ff72
Kenneth-liu-github <84263465+Kenneth-liu-github@users.noreply.github.com>
2026-09-24 21:09:56 +0800

    build: keep reusable Swift package validation

--- Package.swift
+++ Package.swift
@@ -4,7 +4,9 @@ import PackageDescription
 let package = Package(
     name: "MoyashiRecall",
     platforms: [.iOS(.v17)],
-    products: [.library(name: "MoyashiRecall", targets: ["MoyashiRecall"])],
+    products: [
+        .library(name: "MoyashiRecall", targets: ["MoyashiRecall"])
+    ],
     targets: [
         .target(name: "MoyashiRecall"),
         .testTarget(name: "MoyashiRecallTests", dependencies: ["MoyashiRecall"])
