0cca3cebef04853722b663b33fc2f9efc3c4846f
Kenneth-liu-github <84263465+Kenneth-liu-github@users.noreply.github.com>
2026-09-25 18:54:27 +0800

    data: start app with versioned SwiftData container

--- App/MoyashiRecall/MoyashiRecallApp.swift
+++ App/MoyashiRecall/MoyashiRecallApp.swift
@@ -4,16 +4,31 @@ import MoyashiRecall
 
 @main
 struct MoyashiRecallApp: App {
+    private let modelContainer: ModelContainer?
+
+    init() {
+        modelContainer = try? MoyashiRecallModelContainerFactory
+            .makeDefault()
+    }
+
     var body: some Scene {
         WindowGroup {
-            MoyashiRecallRootView()
+            if let modelContainer {
+                MoyashiRecallRootView()
+                    .modelContainer(modelContainer)
+            } else {
+                ContentUnavailableView {
+                    Label(
+                        "无法打开学习数据",
+                        systemImage: "exclamationmark.triangle"
+                    )
+                } description: {
+                    Text(
+                        "Moyashi Recall 无法安全打开本地数据库。"
+                        + " 请不要删除 App；请先保留现有数据并检查迁移问题。"
+                    )
+                }
+            }
         }
-        .modelContainer(for: [
-            SourceDocumentEntity.self,
-            KnowledgeItemEntity.self,
-            FlashcardEntity.self,
-            ReviewStateEntity.self,
-            ReviewHistoryEntity.self
-        ])
     }
 }
